0a2cc004a10244c78119b5dc401bac46d38273b6
HongHa1603 <23701761.ha@student.iuh.edu.vn>
2024-08-16 15:37:29 +0700

    first commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Dang Ha!");
+        System.out.println("Welcome to the LTHDT(420300359105)!");
+    }
+}
